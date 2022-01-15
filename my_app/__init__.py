593234f6a6b86ea874cf562940bb2a5e786f4298
ariasgian <ariasgian@gmail.com>
2022-01-14 23:36:59 -0300

    version alfa ya con el ingreso de las dos tablas

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
-
-
-
 
 app = Flask(__name__)
 
 app.secret_key = 'mysecretkey'
-file= os.getcwd()+'\\database.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lear:1234@localhost/lear' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 db = SQLAlchemy(app)
 from my_app.views import inicial
