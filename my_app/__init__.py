1a5af55a65ca08bfdfd5231f039c5eff46421b74
ariasgian <ariasgian@gmail.com>
2022-01-23 22:39:06 -0300

    git add .

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -6,19 +6,11 @@ cwd = os.getcwd()
 app = Flask(__name__)
 
 app.secret_key = 'mysecretkey'
-
-# connection_string = (
-#     r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
-#     r"DBQ="+ cwd+"\my_app\database.mdb;"
-    
-# )
-
-# connection_uri = f"access+pyodbc:///?odbc_connect={urllib.parse.quote_plus(connection_string)}"
-
+#IP servidor PostgreSQL server
+ip='localhost' 
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lear:1234@localhost/lear' 
-#app.config['SQLALCHEMY_DATABASE_URI'] = connection_uri
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lear:1234@'+ip+'/lear' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 db = SQLAlchemy(app)
 from my_app.views import inicial
