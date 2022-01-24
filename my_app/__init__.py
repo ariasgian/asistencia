99b3e25c3820ecb4d841b00a88fd3fcfbfc77fd4
ariasgian <ariasgian@gmail.com>
2022-01-23 22:40:49 -0300

    test

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -5,8 +5,9 @@ import os
 cwd = os.getcwd()
 app = Flask(__name__)
 
+#tets
 app.secret_key = 'mysecretkey'
-#IP servidor PostgreSQL server
+#IP servidor PostgreSQL server lsd
 ip='localhost' 
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -15,4 +16,3 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 db = SQLAlchemy(app)
 from my_app.views import inicial
 app.register_blueprint(inicial)
-#return app
